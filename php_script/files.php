48927f1305bdd6382e861d7f65259b0cf9e9fca1
klov <klovkrol@yandex.ru>
2013-10-02 22:54:47 +0400

    удаляет все файлы пользователя или добавляет новые

--- php_script/files.php
+++ php_script/files.php
@@ -1,6 +1,20 @@
 ﻿<?php
-include_once("connect_db.php"); 
-if(isset($_GET['file'])&&isset($_GET['user_name']))
+include_once("connect_db.php");
+if(isset($_GET['close'])&&isset($_GET['file'])&&isset($_GET['user_name']))
+{
+		$qure ="SELECT id FROM user WHERE name LIKE '".$_GET['user_name']."'";
+	
+		$result = mysql_query($qure,$db) or die("error select");
+		if(mysql_num_rows($result)>0)
+		{
+			$myrow=mysql_fetch_array($result);
+			
+			$qure = "DELETE FROM files WHERE id_private=".$myrow['id']." AND name LIKE '".$_GET['file']."'"; 
+			$result = mysql_query($qure,$db);
+		}
+			
+}
+else if(isset($_GET['file'])&&isset($_GET['user_name']))
 {		
 		$qure ="SELECT id FROM user WHERE name LIKE '".$_GET['user_name']."'";
 	
