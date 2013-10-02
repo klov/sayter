1e70d03cbc0e9f5a32aefcc6dd7c8c5071ebdbcf
klov <klovkrol@yandex.ru>
2013-10-02 22:47:30 +0400

    возвращает список файлов пользователя или проверяет файл на существование

--- php_script/get_files_name.php
+++ php_script/get_files_name.php
@@ -3,8 +3,23 @@ include_once("connect_db.php");
 error_reporting( E_ERROR );
 header ('Content-Type: application/xml; charset=UTF-8');
 $answer ="<files>";
-if (isset($_GET['name']))
+
+if(isset($_GET['name'])&&isset($_GET['file']))
+	{
+		// добавить фильтрацию входных данных
+		$qure ="SELECT id FROM user WHERE name LIKE '".$_GET['name']."'";
+	$result = mysql_query($qure,$db) or die("error select");
+	$myrow=mysql_fetch_array($result);
+	$result = mysql_query("SELECT * FROM files WHERE id_private=".$myrow['id']."AND name LIKE ".$_GET['file'],$db) or die('ERROR');
+	
+	if(mysql_num_rows($result)>0)
+	{
+		echo "<rezul>1</rezul>";
+	}
+	}
+else if (isset($_GET['name']))
 {
+	// добавить фильтрацию входных данных
 	$qure ="SELECT id FROM user WHERE name LIKE '".$_GET['name']."'";
 	$result = mysql_query($qure,$db) or die("error select");
 	$myrow=mysql_fetch_array($result);
