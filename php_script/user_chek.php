053a584a5ac86fb6faafd528c3a2e491fdadadcc
klov <klovkrol@yandex.ru>
2013-11-08 16:08:40 +0400

    добавлена фильтрация и экранация входных данных

--- php_script/user_chek.php
+++ php_script/user_chek.php
@@ -5,7 +5,14 @@ include_once("connect_db.php");
 
 if(isset($_GET["out"]))
 	{
-		$qure = "SELECT * FROM  user where name like '".$_GET["name"]."'";
+		
+		$input_text = strip_tags($_GET["name"]);
+		$input_text = htmlspecialchars($input_text);
+		$input_text = mysql_escape_string($input_text);
+		
+		$qure = "SELECT * FROM  user where name like '".$input_text."'";
+			
+			
 			
 		$result = mysql_query($qure,$db) or die("error select");
 		if(mysql_num_rows($result)>0)
@@ -17,7 +24,12 @@ if(isset($_GET["out"]))
 					$result = mysql_query($qure,$db) or die("error eror");
 			}
 			while ($myrow=mysql_fetch_array($result));
-			mysql_query("DELETE FROM user WHERE name LIKE '".$_GET["name"]."'",$db) or die("error remmove");
+			
+			$input_text = strip_tags($_GET["name"]);
+			$input_text = htmlspecialchars($input_text);
+			$input_text = mysql_escape_string($input_text);
+			
+			mysql_query("DELETE FROM user WHERE name LIKE '".$input_text."'",$db) or die("error remmove");
 		}
 	}
 	else
@@ -26,7 +38,11 @@ if(isset($_GET["out"]))
 header ('Content-Type: application/xml; charset=UTF-8');
 		if(isset($_GET["name"]))
 		{
-			$qure ="SELECT * FROM  user WHERE name LIKE '".$_GET["name"]."'";
+			$input_text = strip_tags($_GET["name"]);
+			$input_text = htmlspecialchars($input_text);
+			$input_text = mysql_escape_string($input_text);
+			
+			$qure ="SELECT * FROM  user WHERE name LIKE '".$input_text."'";
 	
 			$result = mysql_query($qure,$db) or die("error select");
 			if (mysql_num_rows($result)>0)
@@ -36,7 +52,17 @@ header ('Content-Type: application/xml; charset=UTF-8');
 				}
 				else
 				{	
-					$qure = 'INSERT INTO user(name,URL_adres) VALUES ("'.$_GET["name"].'","'.$_GET["ip"].'")';
+				
+				
+				$input_name = strip_tags($_GET["name"]);
+				$input_text = htmlspecialchars($input_name);
+				$input_text = mysql_escape_string($input_name);
+		
+				$input_ip = strip_tags($_GET["name"]);
+				$input_ip = htmlspecialchars($input_ip);
+				$input_ip = mysql_escape_string($input_ip);
+				
+					$qure = 'INSERT INTO user(name,URL_adres) VALUES ("'.$input_name.'","'.$input_ip.'")';
 		
 					$result = mysql_query($qure,$db) or die("error insert");
 					$answer="<ansver>1</ansver>";
