f3b4da06563083deccf2770e7e58d94a09756cb1
klov <klovkrol@yandex.ru>
2014-01-05 02:50:01 +0600

    begin- начало соеденения с хешированием пароля для передачи. логирование для временой замены до добавления регестрации через соц сеть

--- login.js
+++ login.js
@@ -1,6 +1,17 @@
 // JavaScript Document
+adres_name='ws://192.168.2.33:9000';
 var login ={}
 login.name='';
+acsses_key=[];
+var menu_socet;
+
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 login.in = function()
 {
 	
@@ -8,17 +19,36 @@ login.in = function()
 	$("#my_name").text(us_name);
 	$("#login").val("");
 	var XHRcon= $.ajax({
-		url:"php_script/user_chek.php?name="+us_name+"&ip=192.168.2.33",
+		url:"php_script/user_chek.php?name="+us_name+"&ip="+$("#url").val(),
 		 type:"GET",
 	success:function (date)
 	{
 		var val =$(date).find("ansver");
 		val=val.text();
-		if (val==1)
+		if (val!=0)
 		{
+			var date = new Date;
 			
-			login.name= us_name;
-			$.unblockUI();
+			date.setYear(3040);
+		document.cookie="name="+us_name+";expires="+date.toUTCString();
+	document.cookie="key="+val+";expires="+date.toUTCString();
+			document.cookie="key="+val+"; expires="+date.toUTCString();
+			
+			menu_socet = Socet(adres_name);
+			menu_socet.onclose =function(e)
+			{
+				$.blockUI({ message: $('#login') });
+			}
+			menu_socet.onopen = function(e)
+			{
+				var eve ={
+				name:getCookie("name")
+				};
+				var str= JSON.stringify(eve);
+				menu_socet.send(str);
+			}
+			menu_socet.onmessage = login.begin;
+			
 		} else
 		{
 			alert("такой пользователь уже есть");
@@ -28,3 +58,36 @@ login.in = function()
 	
 }
 	
+	
+login.begin=function(e)
+{
+var cat =JSON.parse(e.data);
+						var k =cat.key;
+						var s=cat.salte;
+						var tyu=getCookie("key");
+						var koj=md5(tyu+s);
+						var hash =md5(md5(getCookie("key")+s)+k);
+						
+						var eve ={
+						name:getCookie("name"),
+						password:md5(md5(getCookie("key")+s)+k)
+						};
+						var str= JSON.stringify(eve);
+						
+							menu_socet.onmessage = function(e)
+							{
+							var cat =JSON.parse(e.data);
+							if(('result' in cat)&&(cat.result=='true'))
+							{
+								$.unblockUI();
+								$("#my_name").val(cat.user).css("visibility","visible");
+								$("#my_name").text(getCookie("name"));
+			login.name= getCookie("name");
+			menu_socet.onmessage = finel_state_socet;
+								
+							}
+							
+							}
+							menu_socet.send(str);		
+					
+			}
