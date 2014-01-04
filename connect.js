3ef95d0e6408faceb0a52fb2a1dd302463238895
klov <klovkrol@yandex.ru>
2014-01-05 02:12:56 +0600

    переработано добаление новых входных сокетов. создана функция add_new_users выполняющая данную фунцию.

--- connect.js
+++ connect.js
@@ -4,6 +4,7 @@
 // и меню
 //////////////////////////////////////////////////////////////////////////////////////
 len_tabs = 0;
+
 var connact =new Object()
  connact.str=undefined;
  connact.pm="";
@@ -11,6 +12,7 @@ connact.dialog_clic=function ()
 { 
 var po;
 var h;
+$("#url").attr("disabled","disabled");
 if (this.str.tagName!=="DIV") {
  po=$(this.str).parent().parent();
 h=$("#name_file").val()+$("#exp").val();
@@ -32,7 +34,7 @@ po=$(po).children(".main_area").children("div:first");
 	var obj='<li><a href="#fragment-'+le+'"><span>'+h+'</span></a><span class="ui-icon ui-icon-close krest" role="presentation"></span></li>';
 	
 	$(po).children("ul").append(obj);
-	obj='<div id="fragment-'+le+'"><div class="kod" id="'+id+'" contenteditable="true" onKeyUp="focus_in(event.keyCode,this)"  onKeyUp="focus_in(event.keyCode,this)" onKeyPress="focus_uot()" onMouseUp="focus_in(40,this,true)" onMouseDown="focus_uot()""></div></div>';
+	obj='<div id="fragment-'+le+'"><div class="kod" id="'+id+'" contenteditable="true" onKeyUp="focus_in(event.keyCode,this,\''+$("#exp").val()+'\')"  onKeyUp="focus_in(event.keyCode,this)" onKeyPress="focus_uot()" onMouseUp="focus_in(40,this,\''+$("#exp").val()+'\')" onMouseDown="focus_uot()""></div></div>';
 	$(po).append(obj);
 	$(po).tabs( "refresh" );
 	
@@ -42,7 +44,15 @@ po=$(po).children(".main_area").children("div:first");
 	  var name_file = $( this ).closest( "li" ).children("a").children("span").text();
       var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
   
-	  out_socet.send("<type>close<file>"+name_file+"<user>"+login.name);
+  		var eve={
+			type:"close",
+			file:name_file,
+			user:login.name
+			};
+  		var str= JSON.stringify(eve);
+		out_socet.send(str);
+		
+	  //out_socet.send("<type>close<file>"+name_file+"<user>"+login.name);
 	  var XHRcon= $.ajax({
 		url:"php_script/files.php?file="+name_file.replace(/\./g,"_")+"&user_name="+login.name+"&close=true",
 		 type:"GET",
@@ -61,7 +71,15 @@ po=$(po).children(".main_area").children("div:first");
 			out_socet= Socet($("#url").val());
 			out_socet.onopen=function(e)
 			{
-				this.send("<type>out<file>"+h+"<user>"+login.name);
+				var eve={
+					type:"out",
+					file:h,
+					user:login.name,
+					password:login.secrid_code
+				};
+  				var str= JSON.stringify(eve);
+				this.send(str);
+				//this.send("<type>out<file>"+h+"<user>"+login.name);
 			$("#connect_status").removeClass("connect_status_off").addClass("connect_status_on").text("соеденение активно");
 			}
 			out_socet.onclose = function(e)
@@ -72,16 +90,27 @@ po=$(po).children(".main_area").children("div:first");
 	}
 	else
 	{
-		out_socet.send("<type>out<file>"+h+"<user>"+login.name);
+		var eve={
+			type:"out",
+			file:h,			//разобраться с экранированием 
+			user:login.name
+			};
+  		var str= JSON.stringify(eve);
+		out_socet.send(str);
+		//out_socet.send("<type>out<file>"+h+"<user>"+login.name);
 	}
 	$.unblockUI();
 	
 	
 	}
+	
+	
+	
+	
 	connact.connect_p2p=function(param)
 {
 	this.pm=param;				//добавление новых вкладок или выделение существующих
-	var usr =document.getElementById('tabs-'+param.user_id);
+	var usr =document.getElementById('tabs-'+param.user_name);
 	
 	if(usr != null)
 	{
@@ -90,117 +119,110 @@ po=$(po).children(".main_area").children("div:first");
 		var li="";
 		var div="";
 	
-		in_socet[param.user_name]=  Socet("ws://"+param.url+":9002");
+	var eve ={
+	from_user:getCookie("name"),
+	fo_user:param.user_name,
+	ask:'get_connect'
+	};
+	var str= JSON.stringify(eve);
+	menu_socet.send(str);
+	}
+
+
+
 	
-	in_socet[param.user_name].onclose = function(e)
+	connact.add_new_users =function(param)
 	{
-		$("#"+param.user_name).parent().remove();
-		delete in_socet[param.user_name];
+	acsses_key[param.from_user]=param.acsses_key;		
+		if(!(param.from_user in in_socet))
+		in_socet[param.from_user]=  Socet(param.adres);
+	
+	in_socet[param.from_user].onclose = function(e)
+	{
+		$("#"+param.from_user).parent().remove();
+		delete in_socet[param.from_user];
 	}
 	
-	in_socet[param.user_name].onopen = function(e)
+	
+	in_socet[param.from_user].onopen = function(e)
 	{
-		var XHRcon= $.ajax({
-		url:"php_script/get_files_name.php?name="+param.user_name,
-		 type:"GET",
-		 async:false,
-		 success: function(date){
-			 if ( $(date).find("file").length>0)
-			 {
-			 $(date).find("file").each(function(index, element) {
+		var eve ={
+	type:"in",
+	user:param.from_user,
+	password:acsses_key[param.from_user]
+	};
+	var str= JSON.stringify(eve);
+	in_socet[param.from_user].send(str);
+		//in_socet[param.from_user].send("<type>in<file><user>"+param.from_user);
+		for(var i=0; i<param.table.length;i++){
 				 
-				 in_socet[param.user_name].send("<type>in<file>"+$(this).text()+"<user>"+param.user_name);
-				 var kl=$(this).text();
-				li+='<li><a href="#'+$(this).text()+param.user_name+'"><span>'+$(this).text().replace(/_/,".")+'</span></a><span class="ui-icon ui-icon-close krest" role="presentation"></span></li>';
-				div+='<div  id="'+$(this).text()+param.user_name+'" ><div class="frame"></div></div>';
+				 var kl=param.table[i];
+				li+='<li><a href="#'+param.table[i].replace(/\./,"_")+param.from_user+'"><span>'+param.table[i].replace(/_/,".")+'</span></a><span class="ui-icon ui-icon-close krest" role="presentation"></span></li>';
+				div+='<div  id="'+param.table[i].replace(/\./,"_")+param.from_user+'" ><div class="frame"></div></div>';
 				
-            });
-		 var po='<div id="container" class="window"><div class="menu-window "><span class="ui-icon ui-icon-circle-close button " title="выход"></span></div><div id="'+param.user_name+'"><ul>'+li+'</ul>'+div+'</div>';
+		}
+		 var po='<div id="container" class="window"><div class="menu-window "><span class="ui-icon ui-icon-circle-close button " title="выход"></span></div><div id="'+param.from_user+'"><ul>'+li+'</ul>'+div+'</div>';
 	$(".menu").after(po);
 	
 	$("body>#container").draggable({ containment:"window",scroll: false, stack:"body>#container",handle:".menu-window"});
-	 var tabs= $("#"+param.user_name).tabs();												// удаление вкладок
+	 var tabs= $("#"+param.from_user).tabs();												// удаление вкладок
 	 tabs.delegate( "span.ui-icon-close", "click", function() {
        $( this ).closest( "li" ).remove();
-      //$( "#" + panelI ).remove();
       tabs.tabs( "refresh" );
-    });	
-			 }
-			 
-		 }
-		 });
-		 
-		
+	 });
+			
 	}
 	
 	
 	
-	in_socet[param.user_name].onmessage= function(e)
+	
+	in_socet[param.from_user].onmessage= function(e)
 	{
 		
-	
-		s =e.data;
-		if((s.search(/<user>/)==0)&&
-		(s.search(/<numbe>/)>0)&&
-		(s.search(/<length>/)>0)&&
-		(s.search(/<file>/)>0)&&
-		(s.search(/<date>/)>0)&&
-		(s.search(/<user>/)<s.search(/<numbe>/))&&
-		(s.search(/<numbe>/)<s.search(/<length>/))&&
-		(s.search(/<length>/)<s.search(/<file>/))&&
-		(s.search(/<file>/)<s.search(/<date>/))
-		)
-		{
-			
-			var mas_date = s.split(/<user>|<date>|<numbe>|<length>|<file>/);
-			
-			var div=$("#"+mas_date[4]+mas_date[1]).children(".fram");
-			if(div.children("div").length!=mas_date[3])
+		
+			var s =e.data;
+					
+			var mas_date = JSON.parse(s);   //.split(/<user>|<date>|<numbe>|<length>|<file>/);
+			if('date' in mas_date){			
+			var div=$("#"+mas_date.file+mas_date.user).children(".fram");
+			if(div.children("div").length!=mas_date.len)
 			{
 				
-			for(var i=0;i<mas_date[3]-div.length; i++)
-			{
-				
-				$("#"+mas_date[4]+mas_date[1]).children(".frame").append("<div></div>");
-			
-			}
-			}
-			var y=$("#"+mas_date[4]+mas_date[1]).children(".frame").children("div:eq("+mas_date[2]+")");
-			y.text(mas_date[5]);
-	
-		}
-		else if(s.search("<type>")==0&&
-             s.search("<file>")>0&&
-             s.search("<user>")>0&&
-             s.search("<type>")<s.search("<file>")&&
-             s.search("<file>")<s.search("<user>"))
+				for(var i=0;i<mas_date.len-div.length; i++)
 				{
-					var mas_date = s.split(/<type>|<file>|<user>/);
-					if(mas_date[1]=="out")
+					var tr=$("#"+mas_date.file+mas_date.user).children(".frame");
+					$("#"+mas_date.file+mas_date.user).children(".frame").append("<div></div>");
+			
+				}
+			}
+			var y=$("#"+mas_date.file+mas_date.user).children(".frame").children("div:eq("+mas_date.numbe+")");
+			y.text(mas_date.date);
+			} else if('type' in mas_date){
+					//var mas_date = s.split(/<type>|<file>|<user>/);
+					if(mas_date.type=="out")
 					{
 				
 				
-							var obj='<li><a href="#'+mas_date[2].replace(/\./g,"_")+mas_date[3]+'"><span>'+mas_date[2]+'</span></a><span class="ui-icon ui-icon-close krest" role="presentation"></span></li>';
+							var obj='<li><a href="#'+mas_date.file.replace(/\./g,"_")+mas_date.user+'"><span>'+mas_date.file+'</span></a><span class="ui-icon ui-icon-close krest" role="presentation"></span></li>';
 	
-							$("#"+mas_date[3]).children("ul").append(obj);
-							obj='<div  id="'+mas_date[2].replace(/\./g,"_")+mas_date[3]+'" class="frame"></div>';
-							$("#"+mas_date[3]).append(obj);
-							$("#"+mas_date[3]).tabs( "refresh" );
+							$("#"+mas_date.user).children("ul").append(obj);
+							obj='<div  id="'+mas_date.file.replace(/\./g,"_")+mas_date.user+'" class="frame"></div>';
+							$("#"+mas_date.user).append(obj);
+							$("#"+mas_date.user).tabs( "refresh" );
 	
 						
 					}
-					else if(mas_date[1]=="close")
+					else if(mas_date.type=="close")
 					{
-						delete in_socet[mas_date[3]];
-						$("a[href='#"+mas_date[2].replace(/\./,"_")+mas_date[3]+"']").closest( "li" ).remove();
-						$("#"+mas_date[2].replace(/\./,"_")+mas_date[3]).remove();
+						//delete in_socet[mas_date[3]];
+						var yu="a[href='#"+mas_date.file.replace(/\./,"_")+mas_date.user+"']";
+						$("a[href='#"+mas_date.file.replace(/\./,"_")+mas_date.user+"']").closest( "li" ).remove();
+						$("#"+mas_date.file.replace(/\./,"_")+mas_date.user).remove();
 					}
 				}
 						
-				
-	
-	}			
 
 	
+	}
 	}
 }
