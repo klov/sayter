6b1cc5ae03416c618ae92c0de688b49d595a6d9b
klov <klovkrol@yandex.ru>
2014-06-04 21:14:22 +0600

    изменен запрос к базе данных

--- U_and_G.php
+++ U_and_G.php
@@ -4,27 +4,18 @@ include_once("php_script/connect_db.php");
 header ('Content-Type: application/xml; charset=UTF-8');
 $answer ='<?xml version="1.0" ?>';
 $answer ="<users>";
-$result = mysql_query("SELECT name,url,id FROM user_online",$db) or die('ERROR');
+$result = mysql_query("SELECT name,id FROM user_online",$db) or die('ERROR');
 if(mysql_num_rows($result)>0){
 $myrow=mysql_fetch_array($result);
 do
 {
-$answer.="<user><name>".$myrow["name"]."</name><URL>".$myrow["url"]."</URL><id>".$myrow['id']."</id></user>";
+$answer.="<user><name>".$myrow["name"]."</name><id>".$myrow['id']."</id></user>";
 }
 while ($myrow=mysql_fetch_array($result));
 }else
 {
 }
-$result = mysql_query("SELECT * FROM `group`",$db) or die('ERROR2');
-$myrow=mysql_fetch_array($result);
-if(mysql_num_rows($result)>0){
-do
-{
-$answer.='<group><name>'.$myrow['name'].'</name><id>'.$myrow['id'].'</id></group>';	
-}
-while ($myrow=mysql_fetch_array($result));
-}else{
-	}
+
 $answer.="</users>";
 echo $answer;
 
